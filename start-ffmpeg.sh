bb84070bc2b2a1e9054f521dc95d64b978d9b04e
Brett VanderHaar <bvanderhaar@users.noreply.github.com>
2017-06-18 18:43:47 -0400

    Create start-ffmpeg.sh

---
+++ start-ffmpeg.sh
@@ -0,0 +1,1 @@
+ffmpeg -v verbose -i rtsp://admin:admin@192.168.23.80:554 -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 /srv/http/cam1.m3u8
